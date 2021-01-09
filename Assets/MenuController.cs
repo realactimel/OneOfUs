a51b5389edf19b24988c340f3b55afe335341d30
RealActimel <Activelione@gmail.com>
2021-01-09 23:32:01 +0100

    Neu anfang OneOfUs 09.01.2021/ 23:31

---
+++ Assets/MenuController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField] private string VersioName = "DevBuild-0.1";
+    [SerializeField] private GameObject Usernamewindow;
+    [SerializeField] private GameObject ConnectPanel;
+
+    [SerializeField] private InputField UsernameInput;
+    [SerializeField] private InputField CreateGameInput;
+    [SerializeField] private InputField JoinGameInput;
+
+     [SerializeField] private GameObject StartButton;
+
+
+    private void Awake()
+    {
+        PhotonNetwork.ConnectUsingSettings(VersioName);
+    }
+
+    private void Start()
+    {
+        Usernamewindow.SetActive(true);
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+        Debug.Log("Connected");
+    }
+
+    public void ChangeUserNameInput()
+    {
+        if(UsernameInput.text.Length >= 3)
+        {
+            StartButton.SetActive(true);
+        } 
+        else 
+        {
+            StartButton.SetActive(false);
+        }
+    }
+
+    public void SetUserName()
+    {
+        Usernamewindow.SetActive(false);
+        PhotonNetwork.playerName = UsernameInput.text;
+    }
+
+    public void CreateGame()
+    {
+        PhotonNetwork.CreateRoom(CreateGameInput.text, new RoomOptions() { maxPlayers = 5 }, null);
+    }
+
+    public void JoinGame()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.maxPlayers = 5;
+        PhotonNetwork.JoinOrCreateRoom(JoinGameInput.text, roomOptions, TypedLobby.Default);
+    }
+}
+// by Lion & Max ~ 09.01.2021
